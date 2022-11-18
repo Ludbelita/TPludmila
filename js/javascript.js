33986e5e2139b3dc5dd24ad5899296d552321304
Ludbelita <106352162+Ludbelita@users.noreply.github.com>
2022-11-17 23:54:57 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+
+
+function noche() {
+  document.body.style.backgroundImage = "none";
+document.body.style.backgroundColor = "#1F1E21";
+document.getElementById("barra").style.backgroundColor = "#312E2E";
+document.getElementById("link1").style.color = "white";
+document.getElementById("link2").style.color = "white";
+document.getElementById("link3").style.color = "white";
+document.getElementById("link4").style.color = "white";
+document.getElementById("link5").style.color = "white";
+document.getElementById("1").style.color = "#FFFFFF";
+document.getElementById("2").style.background = "none";
+document.getElementById("2").style.color = "#FFFFFF";
+document.getElementById("3").style.background = "none";
+document.getElementById("3").style.color = "#FFFFFF";
+document.getElementById("cartas").style.backgroundColor = "#312E2E";
+  }
+
+
+function cartelinfo() { alert( "Autor: Ludmila Martinez  Correo: Ludmilam.martinez@outlook.com")
+
+}
